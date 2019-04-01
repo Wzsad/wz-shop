9c5717a797f48ab6a8e3938c830c8270038cea95
vagrant <vagrant@homestead>
2019-04-01 02:54:43 +0000

    邮箱验证

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// 必须得验证邮箱的用户才可以访问添加中间件，laravel自带的验证方法添加->middleware('verified')
 Route::get('/', 'PagesController@root')->name('root');
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
